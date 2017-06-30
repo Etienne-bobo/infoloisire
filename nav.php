138a937a1ce06e351fccee44b7fb315d9fdeac59
lezarius <remy.mazabraud@gmail.com>
2017-06-30 12:16:10 +0200

    tempo logo

--- nav.php
+++ nav.php
@@ -14,7 +14,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.php">InfoLoisire</a>
+      <a class="" href="index.php">InfoLoisire</a>
     </div>
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="top-navbar-1">
