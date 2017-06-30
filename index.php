a4fac816131a6e7c80e9db6c97f99eb87b69d282
lezarius <remy.mazabraud@gmail.com>
2017-06-30 11:52:25 +0200

    add content from client

--- index.php
+++ index.php
@@ -39,24 +39,64 @@
       <h1>Bienvenue sur infoloisire</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum ligula a lacus pellentesque, non varius lacus tincidunt. In eu. </p>
       <div class="page-title-bottom-link">
-        <a href="prestation.php">Prestation</a>
-        <a href="contact.php">Contactez-nous</a>
+        <a class="big-link-2 btn" href="prestation.php">Prestation</a>
+        <a class="big-link-2 btn scroll-link" href="#what-we-do">En apprendre plus</a>
       </div>
     </div>
 </div>
 <!-- What we do -->
+  <div class="block-3-container section-container what-we-do-container">
       <div class="container">
           <div class="row">
               <div class="col-sm-12 block-3 section-description wow fadeIn">
                   <h2>What we do</h2>
                   <div class="divider-1 wow fadeInUp"><span></span></div>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+
+Infoloisir est une micro entreprise que j'ai fondée pour pouvoir facturer ce que j'ai
+fait toute ma vie gratuitement : Aider les gens en informatique.
+
+Je ne parle pas seulement de dépanage, mais aussi d'installations de systèmes de loisirs
+ou de sauvegardes de leurs informations (Voir la page des services proposés).
+
+Vous pouriez vous poser la question: Si j'ai passé de très nombreux weeks ends
+chez les voisins et amis sans jamais leur demander quoi que ce soit en échange,
+pourquoi  soudainement vouloir facturer ces services ?
+
+La réponse est simple. Il ne s'agit pas d'un besoin d'argent, mais d'un problème
+de temps. J'ai maintenant passé la quarentaine. J'ai une vie bien remplie (enfants etc).
+Et je ne peux malheuresement plus me permettre de dépanner tout le monde sans contrepartie.
+
+Alors si vous avez besoin d'un passioné pour vous former aux "danger d'internet" ou à son
+fonctionnement. Ou d'une personne capable de vous expliquer (et/ou vous montrer) ce que vous
+pourriez avoir chez vous en matière d'informatique, de multimédia, ou de domotique../
+D'une personne capable de s'adapter à vos besoins. De vous fournir clef en mains des solutions
+avec les formations nécéssaires pour les entrenir dans le temps...
+N'hesitez pas à faire appel à moi.
+
+Libriste convaincu, je vous orienterai systématiquement vers les solutions les moins
+dangereuses pour vos données et votre vie privée. Des solutions qui sont aussi les plus
+fiables et les moins chères (puisque généralement gratuites...)
+
+Comment cela fonctionne :
+
+Vous m'appeler ou m'envoyer un mail avec une heure et votre numéro de téléphone si
+vous voulez*que ce soit moi qui vous appelle. Lors de ce premier échange, vous m'expliquer
+votre besoin.
+
+Si je pense pouvoir vous aider nous échangerons des informations pour que je puisse
+vous établir un devis (gratuit) précis, que je vous enverrai par mail dans les jours
+qui suivent.
+
+Dans le cas contraire, je vous expliquerai pourquoi je ne pourrai pas vous aider.
+Un exemple réccurant concerne les demandes de formations à la suite Office de Microsoft.
+Je suis passioné d'informatique. Utiliser un traitement de texte n'est pas
+"faire de l'informatique". C'est de la bureautique. Et cela n'a absolument rien à voir 
                   </p>
               </div>
           </div>
         </div>
+      </div>
 <?php include 'footer.php'; ?>
     </body>
 
