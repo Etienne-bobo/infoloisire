80957ee93afa716e134e2a7c6a3c5b55ddd01bbb
lezarius <remy.mazabraud@gmail.com>
2017-07-01 15:51:47 +0200

    add potential UI for prestation page

--- prestation.php
+++ prestation.php
@@ -1,1 +1,186 @@
- 
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>InfoLoisire</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:700,300,400">
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/elegant-font/code/style.css">
+        <link rel="stylesheet" href="assets/css/animate.css">
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+   <link rel="stylesheet" href="assets/css/form-elements.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/media-queries.css">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- Favicon and touch icons -->
+
+    </head>
+
+    <body>
+<?php include 'nav.php'; ?>
+
+		<!-- Block 2 -->
+        <div class="block-2-container section-container">
+	        <div class="container">
+	            <div class="row">
+	            	<div class="col-sm-6 block-2-box block-2-left block-2-media wow fadeInLeft">
+	            		<div class="block-2-img-container">
+	            			<img src="assets/img/about/4.jpg" alt="" data-at2x="assets/img/about/4.jpg">
+	            			<div class="img-container-line line-1"></div>
+	            			<div class="img-container-line line-2"></div>
+	            			<div class="img-container-line line-3"></div>
+	            		</div>
+	            	</div>
+	            	<div class="col-sm-6 block-2-box block-2-right wow fadeInUp">
+	            		<h3>We love web design</h3>
+	            		<p>
+	            			Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
+	            			lobortis nisl ut aliquip ex ea commodo consequat.
+	            		</p>
+	            		<ul>
+	            			<li>
+	            				<span aria-hidden="true" class="icon_check list-icon"></span> Etiam processus dynamicus qui
+	            			</li>
+	            			<li>
+	            				<span aria-hidden="true" class="icon_check list-icon"></span> Quam nunc putamus Photoshop
+	            			</li>
+	            			<li>
+	            				<span aria-hidden="true" class="icon_check list-icon"></span> Mutationem consuetudium WordPress
+	            			</li>
+	            			<li>
+	            				<span aria-hidden="true" class="icon_check list-icon"></span> Exerci tation ullamcorper
+	            			</li>
+	            		</ul>
+	            	</div>
+	            </div>
+	        </div>
+        </div>
+
+        <!-- Block 2 -->
+        <div class="block-2-container section-container section-container-gray">
+	        <div class="container">
+	            <div class="row">
+	            	<div class="col-sm-6 block-2-box block-2-left wow fadeInLeft">
+	            		<h3>We design interfaces</h3>
+	            		<p>
+	            			Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
+	            			lobortis nisl ut aliquip ex ea commodo consequat.
+	            		</p>
+	            		<ul>
+	            			<li>
+	            				<span aria-hidden="true" class="icon_check list-icon"></span> Etiam processus dynamicus qui
+	            			</li>
+	            			<li>
+	            				<span aria-hidden="true" class="icon_check list-icon"></span> Quam nunc putamus Photoshop
+	            			</li>
+	            			<li>
+	            				<span aria-hidden="true" class="icon_check list-icon"></span> Mutationem consuetudium WordPress
+	            			</li>
+	            			<li>
+	            				<span aria-hidden="true" class="icon_check list-icon"></span> Exerci tation ullamcorper
+	            			</li>
+	            		</ul>
+	            	</div>
+	            	<div class="col-sm-6 block-2-box block-2-right block-2-media wow fadeInUp">
+	            		<div class="block-2-img-container">
+	            			<img src="assets/img/about/5.jpg" alt="" data-at2x="assets/img/about/5.jpg">
+	            			<div class="img-container-line line-1"></div>
+	            			<div class="img-container-line line-2"></div>
+	            			<div class="img-container-line line-3"></div>
+	            		</div>
+	            	</div>
+	            </div>
+	        </div>
+        </div>
+
+        		<!-- Block 2 -->
+                <div class="block-2-container section-container">
+        	        <div class="container">
+        	            <div class="row">
+        	            	<div class="col-sm-6 block-2-box block-2-left block-2-media wow fadeInLeft">
+        	            		<div class="block-2-img-container">
+        	            			<img src="assets/img/about/4.jpg" alt="" data-at2x="assets/img/about/4.jpg">
+        	            			<div class="img-container-line line-1"></div>
+        	            			<div class="img-container-line line-2"></div>
+        	            			<div class="img-container-line line-3"></div>
+        	            		</div>
+        	            	</div>
+        	            	<div class="col-sm-6 block-2-box block-2-right wow fadeInUp">
+        	            		<h3>We love web design</h3>
+        	            		<p>
+        	            			Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
+        	            			lobortis nisl ut aliquip ex ea commodo consequat.
+        	            		</p>
+        	            		<ul>
+        	            			<li>
+        	            				<span aria-hidden="true" class="icon_check list-icon"></span> Etiam processus dynamicus qui
+        	            			</li>
+        	            			<li>
+        	            				<span aria-hidden="true" class="icon_check list-icon"></span> Quam nunc putamus Photoshop
+        	            			</li>
+        	            			<li>
+        	            				<span aria-hidden="true" class="icon_check list-icon"></span> Mutationem consuetudium WordPress
+        	            			</li>
+        	            			<li>
+        	            				<span aria-hidden="true" class="icon_check list-icon"></span> Exerci tation ullamcorper
+        	            			</li>
+        	            		</ul>
+        	            	</div>
+        	            </div>
+        	        </div>
+                </div>
+
+                <!-- Block 2 -->
+                <div class="block-2-container section-container section-container-gray">
+        	        <div class="container">
+        	            <div class="row">
+        	            	<div class="col-sm-6 block-2-box block-2-left wow fadeInLeft">
+        	            		<h3>We design interfaces</h3>
+        	            		<p>
+        	            			Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
+        	            			lobortis nisl ut aliquip ex ea commodo consequat.
+        	            		</p>
+        	            		<ul>
+        	            			<li>
+        	            				<span aria-hidden="true" class="icon_check list-icon"></span> Etiam processus dynamicus qui
+        	            			</li>
+        	            			<li>
+        	            				<span aria-hidden="true" class="icon_check list-icon"></span> Quam nunc putamus Photoshop
+        	            			</li>
+        	            			<li>
+        	            				<span aria-hidden="true" class="icon_check list-icon"></span> Mutationem consuetudium WordPress
+        	            			</li>
+        	            			<li>
+        	            				<span aria-hidden="true" class="icon_check list-icon"></span> Exerci tation ullamcorper
+        	            			</li>
+        	            		</ul>
+        	            	</div>
+        	            	<div class="col-sm-6 block-2-box block-2-right block-2-media wow fadeInUp">
+        	            		<div class="block-2-img-container">
+        	            			<img src="assets/img/about/5.jpg" alt="" data-at2x="assets/img/about/5.jpg">
+        	            			<div class="img-container-line line-1"></div>
+        	            			<div class="img-container-line line-2"></div>
+        	            			<div class="img-container-line line-3"></div>
+        	            		</div>
+        	            	</div>
+        	            </div>
+        	        </div>
+                </div>
+<?php include 'footer.php'; ?>
+    </body>
+
+</html>
