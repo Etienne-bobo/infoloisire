119afe13ebdbbcd0333555a1e771031cdba2e950
lezarius <remy.mazabraud@gmail.com>
2017-07-01 16:01:26 +0200

    test fixing bug

--- contact.php
+++ contact.php
@@ -32,79 +32,86 @@
 
     <body>
 <?php include 'nav.php'; ?>
+<!-- Contact us (block 2) -->
+    <div class="block-2-container section-container contact-container">
+      <div class="container">
 
-<section id="three" class="wrapper">
-      <div class="inner">
-        <header class="align-center">
-          <h2>Nous contacter</h2>
-          <p>Téléphone ? Mail ? Formulaire ?</p>
-          <p>Faites votre choix et contactez-nous.</p>
-        </header>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="well well-sm">
-                    <form>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="name">
-                                    Nom</label>
-                                <input type="text" class="form-control" id="name" placeholder="Enter name" required="required" />
-                            </div>
-                            <div class="form-group">
-                                <label for="email">
-                                    Email Address</label>
-                                <div class="input-group">
-                                    <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
-                                    </span>
-                                    <input type="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
-                            </div>
-                            <div class="form-group">
-                                <label for="subject">
-                                    Sujet</label>
-                                <select id="subject" name="subject" class="form-control" required="required">
-                                    <option value="na" selected="">En choisir un:</option>
-                                    <option value="service">Service</option>
-                                    <option value="suggestions">Devis</option>
-                                    <option value="product">Autre (datalove, suggestion, etc)</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="name">
-                                    Message</label>
-                                <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
-                                    placeholder="C'est un monde avec des licornes. Oh, heureusement que ce texte va être remplacé."></textarea>
-                            </div>
-                            <button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
-                                Envoyer le message</button>
-                        </div>
-                    </div>
-                    </form>
-                </div>
-            </div>
-            <div class="col-md-4">
-            <img src="images/logo200.png" alt=""></img>
-            </div>
-            <div class="col-md-4">
-                <form>
-                <address>
-                    <strong>Infoloisire</strong><br>
-                    XXXXX<br>
-                    <abbr title="Phone">
-                        P:</abbr>
-                    XXXXX
-                </address>
-                <address>
-                    <strong>XXXXX</strong><br>
-                    <a href="mailto:XXXXX">XXXXX</a>
-                </address>
-                </form>
-            </div>
-        </div>
+              <section id="three" class="wrapper">
+        				<div class="inner">
+        					<header class="align-center">
+        						<h2>Nous contacter</h2>
+        						<p>Téléphone ? Mail ? Formulaire ?</p>
+        						<p>Faites votre choix et contactez-nous.</p>
+        					</header>
+
+              <div class="container">
+                  <div class="row">
+                      <div class="col-md-8">
+                          <div class="well well-sm">
+                              <form>
+                              <div class="row">
+                                  <div class="col-md-6">
+                                      <div class="form-group">
+                                          <label for="name">
+                                              Nom</label>
+                                          <input type="text" class="form-control" id="name" placeholder="Enter name" required="required" />
+                                      </div>
+                                      <div class="form-group">
+                                          <label for="email">
+                                              Email Address</label>
+                                          <div class="input-group">
+                                              <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+                                              </span>
+                                              <input type="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
+                                      </div>
+                                      <div class="form-group">
+                                          <label for="subject">
+                                              Sujet</label>
+                                          <select id="subject" name="subject" class="form-control" required="required">
+                                              <option value="na" selected="">En choisir un:</option>
+                                              <option value="service">Service</option>
+                                              <option value="suggestions">Devis</option>
+                                              <option value="product">Autre (datalove, suggestion, etc)</option>
+                                          </select>
+                                      </div>
+                                  </div>
+                                  <div class="col-md-6">
+                                      <div class="form-group">
+                                          <label for="name">
+                                              Message</label>
+                                          <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
+                                              placeholder="C'est un monde avec des licornes. Oh, heureusement que ce texte va être remplacé."></textarea>
+                                      </div>
+                                      <button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
+                                          Envoyer le message</button>
+                                  </div>
+                              </div>
+                              </form>
+                          </div>
+                      </div>
+                      <div class="col-md-4">
+                      <img src="images/logo200.png" alt=""></img>
+                      </div>
+                      <div class="col-md-4">
+                          <form>
+                          <address>
+                              <strong>RaiseMind</strong><br>
+                              Bordeaux, 33000<br>
+                              <abbr title="Phone">
+                                  P:</abbr>
+                              +337 53 34 54 77
+                          </address>
+                          <address>
+                              <strong>Ronce Mazanraud</strong><br>
+                              <a href="mailto:pro@raisemind.pw">pro@raisemind.pw</a>
+                          </address>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+
+      </div>
     </div>
 <?php include 'footer.php'; ?>
     </body>
